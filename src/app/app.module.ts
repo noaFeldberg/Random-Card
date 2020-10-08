ac6bc5e3b07b7a7ba4038e9230fab6ec8b1bcfa8
noa_feldberg <noa@clarifruit.com>
2020-10-08 20:02:04 +0300

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//http
+import { HttpClientModule } from "@angular/common/http"
+//toaster
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+//font awesome
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
