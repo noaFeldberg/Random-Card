bc562a883ca080b9a26e2187b9b7a1c6dec860e5
noa_feldberg <noa@clarifruit.com>
2020-10-09 14:27:52 +0300

    Add api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'random-card';
+export class AppComponent implements OnInit {
+  title = "random-card";
+  user: any;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUser().subscribe(
+      (user: any) => {
+        this.user = user.results[0];
+      },
+      // (err) => {
+      //   this.toastrService.error(err.status, "Oops")
+      // }
+    )
+
+  }
 }
